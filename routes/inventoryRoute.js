1c8ea84dfe1b8b1d8ff2cf04aab9a58079c4a82a
FrodoPano <frodopano@gmail.com>
2025-09-24 14:39:52 -0600

    utilities

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -1,23 +1,25 @@
 // Needed Resources 
 const express = require("express")
 const router = new express.Router() 
+const utilities = require("../utilities/")
 const invController = require("../controllers/invController")
+const regValidate = require('../utilities/account-validation')
 
-// Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
-
-// Route to build vehicle detail view
-router.get("/detail/:invId", invController.buildVehicleDetail);
-
+// Route to build management view
 router.get("/", utilities.handleErrors(invController.buildManagement))
 
-//classification routes
+// Route to build inventory by classification view
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId))
+
+// Route to build vehicle detail view
+router.get("/detail/:invId", utilities.handleErrors(invController.buildVehicleDetail))
+
+// Add classification routes
 router.get("/add-classification", utilities.handleErrors(invController.buildAddClassification))
 router.post("/add-classification", 
   regValidate.classificationRules(),
   regValidate.checkClassificationData,
   utilities.handleErrors(invController.addClassification))
-
 
 // Add inventory routes
 router.get("/add-inventory", utilities.handleErrors(invController.buildAddInventory))
@@ -26,4 +28,4 @@ router.post("/add-inventory",
   regValidate.checkInventoryData,
   utilities.handleErrors(invController.addInventory))
 
-module.exports = router;
+module.exports = router
